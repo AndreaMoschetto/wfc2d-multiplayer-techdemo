b5ef0f8709aaff660e37559561e61c215e46c0c9
Niria <andreamoschetto99@gmail.com>
2023-08-13 15:25:20 +0200

    add MapTest class

---
+++ src/client/src/ts/scenes/map-test.ts
@@ -0,0 +1,43 @@
+import { Images } from "@root/resources";
+import { Engine, Scene, SceneActivationContext, SpriteSheet, TileMap } from "excalibur";
+
+export class MapTest extends Scene {
+    private tilemap!: TileMap
+    override onInitialize(_engine: Engine): void {
+
+
+        const tilemapSpriteSheet = SpriteSheet.fromImageSource({
+            image: Images.tilemapImage,
+            grid: {
+                rows: 11,
+                columns: 13,
+                spriteHeight: 16,
+                spriteWidth: 16
+            },
+            spacing: {
+                margin: {
+                    x: 0,
+                    y: 0
+                }
+            }
+        });
+
+        this.tilemap = new TileMap({
+            rows: 11,
+            columns: 13,
+            tileWidth: 16,
+            tileHeight: 16,
+        });
+
+        for (let y = 0; y < this.tilemap.rows; y++) {
+            for (let x = 0; x < this.tilemap.columns; x++) {
+                const sprite = tilemapSpriteSheet.getSprite(x, y);
+                this.tilemap.getTile(x,y).addGraphic(sprite!)
+            }
+        }
+    }
+    override onActivate(_context: SceneActivationContext<unknown>): void {
+        this.add(this.tilemap)
+        this.tilemap.scale.setTo(3,3)
+    }
+}
