c0d432ab023202b66c4b203a2ff46d1def4e4956
AndreaMoschetto <andreamoschetto99@gmail.com>
2023-10-14 16:05:40 +0200

    chore: update settings

--- src/client/src/ts/settings.ts
+++ src/client/src/ts/settings.ts
@@ -10,7 +10,7 @@ export const MAP_TEST = 'map-test'
 //WebSocket settings
 export const SERVER_ADDR = 'localhost'
 export const SERVER_PORT = 7777
-export const MAX_USERS = 2
+export const MAX_USERS = 5
 
 //error codes
 export enum ErrorCode{
